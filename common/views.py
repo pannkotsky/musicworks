b984f001f5262b85b14a9a6a3c0c0170320921cf
Valerii Kovalchuk <kovvalole@gmail.com>
2019-09-15 18:15:54 +0300

    Bulk create

--- common/views.py
+++ common/views.py
@@ -1,9 +1,10 @@
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
+from rest_framework_bulk.mixins import BulkCreateModelMixin
 
 
-class RetrieveOrCreateViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
-                              mixins.ListModelMixin, GenericViewSet):
+class RetrieveOrBulkCreateViewSet(BulkCreateModelMixin, mixins.RetrieveModelMixin,
+                                  mixins.ListModelMixin, GenericViewSet):
     """
     A viewset that provides default `create()`, `retrieve()` and `list()` actions.
     """
--- works/views.py
+++ works/views.py
@@ -1,26 +1,26 @@
 from rest_framework.settings import api_settings
 from rest_framework_csv.renderers import CSVRenderer
 
-from common.views import RetrieveOrCreateViewSet
+from common.views import RetrieveOrBulkCreateViewSet
 from works.models import Contributor, Source, Work
 from .filters import WorkFilter
 from .serializers import ContributorSerializer, SourceSerializer, WorkSerializer
 
 
-class ContributorViewSet(RetrieveOrCreateViewSet):
+class ContributorViewSet(RetrieveOrBulkCreateViewSet):
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
     filterset_fields = ['verified']
     search_fields = ['first_name', 'last_name', 'middle_name']
 
 
-class SourceViewSet(RetrieveOrCreateViewSet):
+class SourceViewSet(RetrieveOrBulkCreateViewSet):
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
     search_fields = ['identifier']
 
 
-class WorkViewSet(RetrieveOrCreateViewSet):
+class WorkViewSet(RetrieveOrBulkCreateViewSet):
     queryset = Work.objects.all()
     serializer_class = WorkSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES + [CSVRenderer]
